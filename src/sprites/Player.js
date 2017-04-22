5776a6eb8db919cc8df877669c21808844fa71fb
John Hawthorn <john.hawthorn@gmail.com>
2017-04-22 03:29:49 -0700

    Rotation experiment

--- src/sprites/Player.js
+++ src/sprites/Player.js
@@ -5,7 +5,7 @@ export default class extends Phaser.Sprite {
     this.anchor.setTo(0.5)
 
     game.physics.p2.enable([this], true);
-    this.body.setMaterial(game.planetMaterial);
+    this.body.setMaterial(game.playerMaterial);
 
     this.inputEnabled = true;
     this.cursors = game.input.keyboard.createCursorKeys();
@@ -15,24 +15,47 @@ export default class extends Phaser.Sprite {
 
   update () {
     if(this.cursors.right.isDown) {
-      this.body.x += 3
-    } else if(this.cursors.left.isDown) {
-      this.body.x -= 3
+      this.body.thrustRight(300);
+    }
+    if(this.cursors.left.isDown) {
+      this.body.thrustLeft(300);
     }
 
     if(this.jumpButton.isDown && this.canJump()) {
-      this.body.moveUp(300);
+      this.jump();
     }
   }
 
+  jump() {
+    this.body.moveForward(300);
+  }
+
   canJump() {
-    return true
+    return this.onGround()
+  }
+
+  onGround() {
+    var yAxis = p2.vec2.fromValues(0, 1);
+    var result = false;
+
+    for (var i = 0; i < game.physics.p2.world.narrowphase.contactEquations.length; i++) {
+      var c = game.physics.p2.world.narrowphase.contactEquations[i];
+
+      if (c.bodyA === this.body.data || c.bodyB === this.body.data) {
+        var d = p2.vec2.dot(c.normalA, yAxis); // Normal dot Y-axis
+        if (c.bodyA === this.body.data) d *= -1;
+        if (d > 0.5) result = true;
+      }
+    }
+
+    return result;
   }
 
   accelerateToObject(obj) {
-    var speed = 600;
-    var angle = Math.atan2(obj.y - this.y, obj.x - this.x);
-    this.body.rotation = angle - game.math.degToRad(90);
+    const speed = 600;
+    const angle = Math.atan2(obj.y - this.y, obj.x - this.x);
+    const targetRotation = angle - game.math.degToRad(90);
+    this.body.rotation = targetRotation;
     this.body.force.x = Math.cos(angle) * speed;
     this.body.force.y = Math.sin(angle) * speed;
   }
--- src/states/Game.js
+++ src/states/Game.js
@@ -18,20 +18,25 @@ export default class extends Phaser.State {
     banner.smoothed = false
     banner.anchor.setTo(0.5)
 
-    var starfield = game.add.tileSprite(0, 0, 800, 600, 'starfield');
-    starfield.fixedToCamera = true;
+    game.world.resize(5000, 5000);
+
+    this.starfield = game.add.tileSprite(-1024, -1024, 2048, 2048, 'starfield');
+    this.starfield.fixedToCamera = true;
 
     this.game.physics.startSystem(Phaser.Physics.P2JS);
 
-    this.planetMaterial = game.physics.p2.createMaterial('planetMaterial');
-    this.playerMaterial = game.physics.p2.createMaterial('planetMaterial');
 
-    this.mushroom = new Mushroom({
-      game: this,
-      x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'mushroom'
-    })
+    //game.physics.p2.gravity.y = 100;
+    game.physics.p2.restitution = 0.8;
+
+    this.game.physics.p2.world.defaultContactMaterial.friction = 0.9;
+    this.game.physics.p2.world.setGlobalStiffness(1e5);
+
+    this.planetMaterial = game.physics.p2.createMaterial('planetMaterial');
+    this.playerMaterial = game.physics.p2.createMaterial('playerMaterial');
+
+    var playerPlanetMC = game.physics.p2.createContactMaterial(this.playerMaterial, this.planetMaterial, { friction: 1, frictionStiffness: 1e7, frictionRelaxation: 3 });
+    console.log(playerPlanetMC);
 
     this.player = new Player({
       game: this,
@@ -42,7 +47,7 @@ export default class extends Phaser.State {
     this.game.add.existing(this.player)
     game.camera.follow(this.player);
 
-    const diameter = 200;
+    const diameter = 2000;
     this.planet = new Planet({
       game: this,
       x: this.world.centerX,
@@ -50,15 +55,37 @@ export default class extends Phaser.State {
       diameter: diameter
     });
     this.game.add.existing(this.planet)
+
+    this.worldgroup = this.game.add.group();
+    this.worldgroup.add(this.starfield);
+    this.worldgroup.add(this.player);
+    this.worldgroup.add(this.planet);
+
+    this.updateRotation();
+    this.game.camera.x = this.player.x - this.camera.width / 2;
+    this.game.camera.y = this.player.y - this.camera.height / 2;
+    this.game.camera.follow(this.player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
+  }
+
+  updateRotation() {
+    this.worldgroup.rotation = -1*this.player.rotation;
+    this.worldgroup.pivot.x = this.player.x;
+    this.worldgroup.pivot.y = this.player.y;
+    this.worldgroup.x = this.worldgroup.pivot.x;
+    this.worldgroup.y = this.worldgroup.pivot.y;
   }
 
   update() {
     this.player.accelerateToObject(this.planet);
+
+    //this.updateRotation();
   }
 
   render () {
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.player, 32, 32)
+      this.game.debug.cameraInfo(game.camera, 32, 32);
+      //this.game.debug.spriteInfo(this.player, 32, 32)
+      //this.game.debug.text("on ground? " + this.player.onGround(), 0, 10 );
     }
   }
 }
